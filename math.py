b366f1b8585a1b2411384b60c895127536526c4e
Angela <ambengeangela@gmail.com>
2023-03-31 15:32:37 +0300

    functions

---
+++ math.py
@@ -0,0 +1,19 @@
+def add (a,b):
+    answer = a + b
+    return answer
+
+def subtract(num1,num2):
+    subtraction = num1-num2
+    return subtraction
+
+def divide(num3,num4):
+    division = num3 / num4
+    return division
+
+def multiply(x,y):
+    multiplication = x*y
+    return multiplication
+
+def remainder(y,z):
+    modulus = y % z
+    return modulus
